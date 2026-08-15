cdd769ab05bcc72123ea6fd94c833ffdb4a7bd57
Mohammed Kaif <mohammedkaif91129@gmail.com>
2026-08-15 18:29:21 +0530

    Time: 5001 ms (5%), Space: 19.9 MB (65.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,10 @@
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i in range(len(nums)):
-#             for j in range(i+1, len(nums)):
-#                 if nums[i]+nums[j]==target:
-#                     return [i ,j] 
-                
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
         for i in range(len(nums)):
-            compliment = target - nums[i]
-            if compliment in seen:
-                return [seen[compliment], i]
+            for j in range(len(nums)):
+                if nums[i]==nums[j] and i==j:
+                    continue
+                if nums[i]+nums[j] == target:
+                    return [i,j]
 
-            else:
-                seen[nums[i]] = i
-            
         
