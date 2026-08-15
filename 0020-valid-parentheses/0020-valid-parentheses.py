e79157aed8b9d444728d74ca8550ab8fe1722b22
Mohammed Kaif <mohammedkaif91129@gmail.com>
2026-08-15 20:46:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (91.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        mapping =  {")": "(", "}": "{", "]": "["}
-        for char in s:
-            if char in mapping:
-                if len(stack)==0:
+        obrackets = {"(":")","[":"]","{":"}"}
+        cbrackets = {")":"(","]":"[","}":"{"}
+        braces = []
+        if s[0] in cbrackets:
+            return False
+        elif s[-1] in obrackets:
+            return False
+        else:
+            for b in s:
+                if b in obrackets:
+                    braces.append(b)
+                elif braces and cbrackets[b] == braces[-1]:
+                    braces.pop()
+                else:
                     return False
-                top = stack.pop()
-                if top != mapping[char]:
-                    return False
+                
+        if len(braces) == 0:
+            return True
+        else:
+            return False
+                    
 
-            else:
-                stack.append(char)
 
 
-        return len(stack) == 0                    
+        
