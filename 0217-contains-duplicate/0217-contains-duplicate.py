cafac17ec09575a5b0caa663a474d91fa14d960d
Mohammed Kaif <mohammedkaif91129@gmail.com>
2026-08-15 18:51:51 +0530

    Time: 26 ms (19.76%), Space: 32.2 MB (73.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,17 +1,14 @@
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         set_nums = len(list(set(nums)))
-#         if set_nums == len(nums):
-#             return False
-#         else:
-#             return True
-        
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # if len(set(nums)) == len(nums):
+        #     return False
+        # else:
+        #     return True
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            else:
+                seen.add(nums[i])
+        return False
         
-        if len(set(nums)) == len(nums):
-            return False
-        else:
-            return True
-
